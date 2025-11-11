e38bf4e5574196b35dc2f7b64334055ad14c9f7e
Krunal Shirsath <128675245+Krunal20022002@users.noreply.github.com>
2025-11-11 12:55:06 +0530

    Change app run configuration to host and port

--- app.py
+++ app.py
@@ -284,5 +284,4 @@ def contacts():
     return render_template('contacts.html', contacts=all_contacts)
 
 if __name__ == '__main__':
-    app.run(debug=True)
     app.run(host="0.0.0.0", port=10000, debug=True)
